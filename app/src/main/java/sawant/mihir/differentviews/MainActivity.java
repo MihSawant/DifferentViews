d399e955d34f20af48fc1627ba6a160070a86ba6
Mihir Sawant <mihirssawant06@gmail.com>
2023-01-29 21:44:52 +0530

    intent and image added to intent

--- app/src/main/java/sawant/mihir/differentviews/MainActivity.java
+++ app/src/main/java/sawant/mihir/differentviews/MainActivity.java
@@ -2,13 +2,30 @@ package sawant.mihir.differentviews;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView image;
+    Button buyButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        image = (ImageView) findViewById(R.id.logo);
+        buyButton = (Button) findViewById(R.id.buyBtn);
+
+        buyButton.setOnClickListener(view -> {
+            Intent myIntent = new Intent(view.getContext(), CoffeeList.class);
+
+            myIntent.putExtra("logo", R.mipmap.coffee_cup);
+            if(myIntent.resolveActivity(getPackageManager()) != null){
+                startActivity(myIntent);
+            }
+        });
     }
 }
